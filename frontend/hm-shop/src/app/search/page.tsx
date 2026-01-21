72694c931c85d9e0293348e7d2fcbdad4a8b29ed
Zachaditya <zachaditya@gmail.com>
2026-01-20 21:21:51 -0800

    fixed?

--- frontend/hm-shop/src/app/search/page.tsx
+++ frontend/hm-shop/src/app/search/page.tsx
@@ -2,10 +2,11 @@ import SearchResultsPage from "@/components/SearchResultsPage";
 import { Suspense } from "react";
 
 export default function Page() {
-  return(
+  return (
     <Suspense
       fallback={<div className="mx-auto max-w-6xl px-4 py-10">Loading…</div>}
     >
-     <SearchResultsPage />;
-     </Suspense>
+      <SearchResultsPage />;
+    </Suspense>
+  );
 }
