9e66590d58be8681cd200f03fc7d724c6ebe5cc1
Zachaditya <zachaditya@gmail.com>
2026-01-21 02:03:17 -0800

    fixed everything

--- backend/app/main.py
+++ backend/app/main.py
@@ -39,7 +39,7 @@ app.include_router(auth_router)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000", "https://eshop-hm-dataset-vmzq.vercel.app"],
+    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000", "https://eshop-hm-dataset.vercel.app/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
