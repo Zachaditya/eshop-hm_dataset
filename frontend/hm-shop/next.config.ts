296d885f27c86bfcde3575ed7d1b666e7c10a125
Zachaditya <zachaditya@gmail.com>
2026-01-21 18:30:30 -0800

    fixed some things on next.config.ts

--- frontend/hm-shop/next.config.ts
+++ frontend/hm-shop/next.config.ts
@@ -1,16 +1,31 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   images: {
     remotePatterns: [
+      // local dev images
       {
         protocol: "http",
         hostname: "127.0.0.1",
         port: "8000",
         pathname: "/images/**",
       },
+      // production images from Railway backend
+      {
+        protocol: "https",
+        hostname: "eshop-hmdataset-production.up.railway.app",
+        pathname: "/images/**",
+      },
     ],
+  },
+
+  async rewrites() {
+    return [
+      {
+        source: "/backend/:path*",
+        destination: "https://eshop-hmdataset-production.up.railway.app/:path*",
+      },
+    ];
   },
 };
 
